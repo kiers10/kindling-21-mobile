a023773d2bc9e030c74b5c3c75589d24dd064d11
kiers10 <densmore.kiersten@gmail.com>
2021-07-21 19:47:04 -0400

    Working password reset

---
+++ screens/passwordscreen.js
@@ -0,0 +1,188 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Pressable, TextInput, ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
+import background from '../assets/background.png';
+import airplane from '../assets/airplane.png';
+import './global';
+
+export default class passResetScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: global.email,
+      hasError: false,
+      errMessage: "",
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={background} style={styles.background}>
+          <Text style={styles.title}>Please check your email</Text>
+          <Image source={airplane} style={styles.airplane}></Image>
+          <Text style={styles.subtitle}>A password reset code has been sent to:</Text>
+          <Text style={styles.emailtitle}>{this.state.email}</Text>
+          <Text style={styles.subtitle}>Please type in the code below. Upon success, your password will be changed.</Text>
+          <TextInput
+            style={styles.input}
+            placeholder={"Verification Code"}
+            onChangeText={(val) => {this.setVerifyCode(val)}}
+          />
+          <Text style={styles.tiny} onPress={this.resendVer}>Resend email</Text>
+          <Text style={styles.error}>{this.state.errMessage}</Text>
+          <Pressable style={styles.continueButton} onPress={this.handleClick}>
+            <Text style={styles.buttonText} onPress={this.handleClick}>Continue</Text>
+          </Pressable>
+        </ImageBackground>
+        <StatusBar barStyle="light-content" backgroundColor="white"/>
+      </View>
+    )
+  }
+
+  setVerifyCode = async (val) => {
+    global.verifyCode = val;
+  }
+
+  handleClick = async() => {
+    try {
+      var loginInfo = {
+        code_str: global.verifyCode.trim(),
+        new_password_str: global.password.trim(),
+      }
+      var jsonObj = JSON.stringify(loginInfo);
+
+      const response = await fetch('https://kindling-lp.herokuapp.com/api/reset_password', 
+        {method:'POST', body:jsonObj, headers:{'Content-Type':'application/json'}});
+
+      var res = JSON.parse(await response.text());
+      
+      // User was successfully registered
+      if (res.success_bool == true) {
+        console.log("Password reset successful");
+
+        this.props.navigation.navigate('Login');
+      }
+      else {
+        console.log("Verification code is incorrect");
+        this.setState({ hasError: true });
+        this.setState({ errMessage: "Verification code is incorrect" });
+        return;
+      }
+    }
+    catch {
+      console.log("Something went wrong");
+    }
+  }
+
+  resendVer = async() => {
+    try {
+      var loginInfo = {
+        email_str: global.email.trim(),
+      }
+      var jsonObj = JSON.stringify(loginInfo);
+
+      const response = await fetch('https://kindling-lp.herokuapp.com/api/send_password_reset', 
+        {method:'POST', body:jsonObj, headers:{'Content-Type':'application/json'}});
+
+      var res = JSON.parse(await response.text());
+      
+      // User was successfully registered
+      if (res.success_bool == true) {
+        console.log("Password reset code sent successful");
+      }
+      else {
+        console.log("Email is incorrect");
+        this.setState({ hasError: true });
+        this.setState({ errMessage: "User with this email does not exist" });
+        return;
+      }
+    }
+    catch {
+      console.log("Something went wrong");
+    }
+  }
+}
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    background: {
+      flex: 1,
+      width: "100%",
+      height: "100%",
+      // justifyContent: "center",
+      alignItems: "center"
+    },
+    title: {
+      fontSize: 30,
+      color: "white",
+      fontWeight: "bold",
+      paddingTop: 125
+    },
+    subtitle: {
+      fontSize: 18,
+      color: "white",
+      fontWeight: "bold",
+      paddingTop: 35,
+      textAlign: "center",
+    },
+    emailtitle: {
+      fontSize: 18,
+      color: "#F4D06F",
+      fontWeight: "bold",
+      paddingTop: 10
+    },
+    airplane: {
+      marginTop: 30,
+      width: 175,
+      height: 175
+    },
+    input: {
+      backgroundColor: '#fff',
+      height: 50,
+      paddingLeft: 15,
+      marginTop: 25,
+      borderWidth: 1,
+      borderRadius: 1,
+      width: "75%",
+      borderRadius: 10,
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 18,
+      fontWeight: "bold"
+    },
+    continueButton: {
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 50,
+      backgroundColor: '#BF4342',
+      height: 45,
+      width: 175,
+      borderRadius: 14,
+  
+      shadowColor: '#CB1809', // IOS
+      shadowOffset: { height: 1, width: 1 }, // IOS
+      shadowOpacity: 1, // IOS
+      shadowRadius: 4, //IOS
+      elevation: 2, // Android
+    },
+    tiny: {
+      fontSize: 15,
+      color: "#F4D06F",
+      fontWeight: "bold",
+      paddingTop: 15
+    },
+    error: {
+      textAlign: "center",
+      marginTop: 20,
+      color: "#E96C6B",
+      fontWeight: "bold",
+      fontSize: 18,
+    },
+  });
---
+++ screens/resetscreen.js
@@ -0,0 +1,127 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Pressable, TextInput, ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
+import background from '../assets/background.png';
+import './global';
+
+export default class resetScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      errMessage: "",
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={background} style={styles.background}>
+          <Text style={styles.title}>Create your new password</Text>
+          <TextInput
+              style={styles.input}
+              placeholder={"Password"}
+              onChangeText={(val) => {this.setPassword(val)}}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder={"Confirm Password"}
+                onChangeText={(val) => {this.setConfirmPassword(val)}}
+            /> 
+          <Text style={styles.error}>{this.state.errMessage}</Text>
+          <Pressable style={styles.continueButton} onPress={this.checkPassword}>
+            <Text style={styles.buttonText} onPress={this.checkPassword}>Reset password</Text>
+          </Pressable>
+        </ImageBackground>
+        <StatusBar barStyle="light-content" backgroundColor="white"/>
+      </View>
+    )
+  }
+
+  setPassword = async (val) => {
+    global.password = val;
+  }
+
+  setConfirmPassword = async (val) => {
+    global.confirmPass = val;
+  }
+
+  checkPassword = () => {
+    // The entered passwords match
+    if (password.localeCompare(confirmPass) == 0) {
+      // Move to reset code verication screen
+      this.props.navigation.navigate('PassReset');
+    }
+    // The passwords do not match
+    else {
+      console.log("Passwords do not match");
+      this.setState({ errMessage: "Passwords do not match" });
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    // justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
+    paddingTop: 125
+  },
+  subtitle: {
+    fontSize: 15,
+    color: "white",
+    // fontWeight: "bold",
+    paddingTop: 15
+  },
+  input: {
+    backgroundColor: '#fff',
+    height: 50,
+    paddingLeft: 15,
+    marginTop: 25,
+    borderWidth: 1,
+    borderRadius: 1,
+    width: "75%",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  continueButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: '#BF4342',
+    height: 45,
+    width: 175,
+    borderRadius: 14,
+
+    shadowColor: '#CB1809', // IOS
+    shadowOffset: { height: 1, width: 1 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 4, //IOS
+    elevation: 2, // Android
+  },
+  error: {
+    textAlign: "center",
+    marginTop: 25,
+    color: "#E96C6B",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
