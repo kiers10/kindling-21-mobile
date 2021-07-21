5e0eefbb47b0af967d080c8a77cd1305e318e375
Joseph Yim <yimsjoseph@knights.ucf.edu>
2021-07-20 22:04:16 -0400

    fucking stupid

--- screens/mainscreen.js
+++ screens/mainscreen.js
@@ -7,40 +7,77 @@ import background from '../assets/background.png';
 import person from '../assets/Person.png';
 import logo from '../assets/FireLogo.png';
 import menu from '../assets/Burger.png';
+import reject from '../assets/reject.png';
+import accept from '../assets/accept.png';
 
+// Sample JSON data for matches 
 let foo = {
   "success": true,
   "groupData": [{
     "title": "Website",
-    "contact": "email1@mail.com",
+    "email": "email1@mail.com",
     "phone": "407-999-9999"
   }, {
       "title": "Website",
-      "contact": "email2@mail.com",
+      "email": "email2@mail.com",
       "phone": "407-999-9999"
     }, {
       "title": "Website",
-      "contact": "email3@mail.com",
+      "email": "email3@mail.com",
       "phone": "407-999-9999"
     }, {
       "title": "Website",
-      "contact": "email4@mail.com",
+      "email": "email4@mail.com",
       "phone": "407-999-9999"
-    },],
+    }, {
+    "title": "Website",
+    "email": "email5@mail.com",
+    "phone": "407-999-9999"
+  }, {
+    "title": "Website",
+    "email": "email6@mail.com",
+    "phone": "407-999-9999"
+  }, {
+    "title": "Website",
+    "email": "email7@mail.com",
+    "phone": "407-999-9999"
+  }, {
+    "title": "Website",
+    "email": "email8@mail.com",
+    "phone": "407-999-9999"
+  }, {
+    "title": "Website",
+    "email": "email9@mail.com",
+    "phone": "407-999-9999"
+  }, {
+    "title": "Website",
+    "email": "email10@mail.com",
+    "phone": "407-999-9999"
+  },],
   "new_token": "prank_on_my_crazy_neighbor"
 }
 
+// Sample JSON data for candidates to be displayed on the card
+let cand = {
+  "success": true,
+  "display_name_str": "Amazon Price Tracking Website",
+  "description_str": "Looking for people that can help make a website for a local business",
+  "phone_str": "407-999-1234",
+  "refreshed_token_str": "testCrazy"
+}
+
+// Empty object array for match
 // NOTE: this needs to be here since this creates the object array used in the 
 // matches list (flatlist)
-let crazy = [];
+let matches = [];
 
 for (let i = 0; i < foo.groupData.length; i++)
 {
   let fifi = new Object;
   fifi.title = foo.groupData[i].title;
-  fifi.contact = foo.groupData[i].contact;
+  fifi.email = foo.groupData[i].email;
   fifi.phone = foo.groupData[i].phone;
-  crazy.push(fifi);
+  matches.push(fifi);
 }
 
 // This is just how a sample data (object array) would look like that is used in the 
@@ -79,6 +116,9 @@ export default class mainScreen extends Component {
       errMessage: "",
       settingModalVisible: false,
       matchModalVisible: false,
+      tempName: "",
+      tempPhone: "",
+      tempDescription: "",
     };
   }
 
@@ -86,7 +126,7 @@ export default class mainScreen extends Component {
     return(
       <View style={{ borderBottomWidth: 1, borderTopWidth: 1, borderColor: "gray", }}>
         <Text style={{ color: "#BF4342", fontWeight: "bold" }}>{item.title}</Text>
-        <Text>{item.contact}</Text>
+        <Text>{item.email}</Text>
         <Text>{item.phone}</Text>
       </View>
     )
@@ -98,6 +138,76 @@ export default class mainScreen extends Component {
 
   setMatchModalVisible = (visible) => {
     this.setState({...Component, matchModalVisible: visible});
+  }
+
+  setTempName = async (val) => {
+    this.tempName = val;
+    console.log(this.tempName);
+  }
+
+  setTempPhone = async (val) => {
+    this.tempPhone = val;
+    console.log(this.tempPhone);
+  }
+
+  setTempDescription = async (val) => {
+    this.tempDescription = val;
+    console.log(this.tempDescription);
+  }
+
+  handleClick = async() => {
+    try {
+      global.fullName = ((this.tempName == "") ? global.fullName : this.tempName.trim());
+      global.phone = ((this.tempPhone == "") ? global.phone : this.tempPhone.trim());
+      global.description = ((this.tempDescription == "") ? global.description : this.tempDescription.trim());
+
+      console.log(global.fullName);
+      console.log(global.phone);
+      console.log(global.description);
+
+      console.log(this.tempName == "");
+      console.log(this.tempPhone == "");
+      console.log(this.tempDescription == "");
+
+      let updateInfo = {
+        email_str: global.email.trim(),
+        update_fields_obj: {
+          display_name_str: global.fullName.trim(),
+          phone_str: global.phone.trim(),
+          description: global.description.trim(),
+        },
+        access_token_str: global.accessToken, 
+      }
+
+      let jsonObj = JSON.stringify(updateInfo);
+
+      const response = await fetch('https://kindling-lp.herokuapp.com/api/update_profile', 
+      {method:'POST', body:jsonObj, headers:{'Content-Type':'application/json'}});
+
+      var res = JSON.parse(await response.text());
+
+      console.log("success boolean: " + res.success_bool);
+      console.log(res.refreshed_token_str);
+
+      if (res.success_bool == true) {
+        global.accessToken = res.refreshed_token_str;
+        global.fullName = (this.tempName === undefined) ? global.fullName : this.tempName.trim();
+        global.phone = (this.tempPhone === undefined) ? global.phone : this.tempPhone.trim();
+        global.description = (this.tempDescription === undefined) ? global.description : this.tempDescription.trim();
+
+        let nameArr = global.fullName.split(" ");
+        global.firstName = nameArr[0];
+        global.lastName = nameArr[1];
+
+        console.log("Profile info updated successfully");
+      }
+      else {
+        console.log("Profile info updated failed :(");
+      }
+    }
+    catch{
+      console.log("something went wrong with the update_profile API");
+    }
   }
 
   render() {
@@ -131,15 +241,15 @@ export default class mainScreen extends Component {
 
                 <View style={styles.modalContent}>
                   <Text style={styles.modalHeader}>Name</Text>
-                  <TextInput style={styles.modalInput} placeholder={this.state.name}></TextInput>
+                  <TextInput style={styles.modalInput} placeholder={this.state.name} onChangeText={(val) => {this.setTempName(val)}}></TextInput>
                   <Text style={styles.modalHeader}>Phone Number</Text>
-                  <TextInput style={styles.modalInput} placeholder={this.state.phone} keyboardType={"number-pad"}></TextInput>
+                  <TextInput style={styles.modalInput} placeholder={this.state.phone} keyboardType={"number-pad"} onChangeText={(val) => {this.setTempPhone(val)}}></TextInput>
                   <Text style={styles.modalHeader} placeholder={this.state.description}>Description</Text>
-                  <TextInput style={styles.modalDescription} multiline={true}></TextInput>
+                  <TextInput style={styles.modalDescription} multiline={true} onChangeText={(val) => {this.setTempDescription(val)}}></TextInput>
 
                   <View style={styles.buttonModal}>
                     <Pressable style={styles.button}>
-                      <Text style={styles.buttonText}>Save Changes</Text>
+                      <Text style={styles.buttonText} onPress={this.handleClick}>Save Changes</Text>
                     </Pressable>
                     <Pressable style={styles.button}>
                       <Text style={styles.buttonText}>Log Out</Text>
@@ -161,18 +271,27 @@ export default class mainScreen extends Component {
                   </Pressable>
                 </View>
 
-                <FlatList 
-                  data={crazy}
+                <FlatList style={styles.flatListStyle}
+                  data={matches}
                   renderItem={this.renderItem}
-                  keyExtractor={item => item.contact}
+                  keyExtractor={item => item.email}
                 />
               </View>
             </View>
           </Modal>
           
-          <View style={{ backgroundColor: "#BF4342", width: "85%", height: "65%", marginTop: "10%" }}>
-            <Text style={{ color: "white" }}>This is a test</Text>
-            <Text style={{ color: "#fff" }}>Kiersten is so cute</Text>
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>{cand.display_name_str}</Text>
+            <Text style={styles.cardDescription}>{cand.description_str}</Text>
+          </View>
+
+          <View style={styles.buttonRow}>
+            <Pressable style={styles.rejectButton}>
+              <Image source={reject}></Image>
+            </Pressable>
+            <Pressable style={styles.acceptButton}>
+              <Image source={accept}></Image>
+            </Pressable>
           </View>
         </ImageBackground>
         <StatusBar barStyle="light-content" backgroundColor="white"/>
@@ -293,5 +412,46 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     marginTop: 5,
+  },
+  card: {
+    backgroundColor: "#BF4342", 
+    width: "85%", 
+    height: "65%", 
+    marginTop: "10%",
+    borderRadius: 10,
+    padding: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  flatListStyle: {
+    height: "95%",
+  },
+  cardTitle: {
+    color: "white",
+    fontSize: 26,
+    fontWeight: "bold",
+    alignContent: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  cardDescription: {
+    color: "white",
+    fontSize: 15,
+    alignContent: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    marginTop: 10,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    marginTop: 20,
+    marginRight: 20,
+  },
+  rejectButton: {
+    marginRight: "45%",
+  },
+  acceptButton: {
+    marginTop: 15,
   }
 });
